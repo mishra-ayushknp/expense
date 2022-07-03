463755bee64e73cec9a4a8f50dab1b75e9ed8685
mishra-ayushknp <ayushmishra.vsmps@gmail.com>
2022-07-03 14:02:26 +0530

    Updates

---
+++ precache-manifest.11f7277660bea67f6da6868f3462aab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c2d87f06c03a066f19e15016d0ce7b",
+    "url": "/expense/index.html"
+  },
+  {
+    "revision": "8f2a7f55239c448eb47a",
+    "url": "/expense/static/css/main.caef1c1c.chunk.css"
+  },
+  {
+    "revision": "130c33d1fbe59508e2dd",
+    "url": "/expense/static/js/2.cdcf560b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense/static/js/2.cdcf560b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2a7f55239c448eb47a",
+    "url": "/expense/static/js/main.fb46a04e.chunk.js"
+  },
+  {
+    "revision": "b07ff3dc4dca91a503a4",
+    "url": "/expense/static/js/runtime-main.f342c104.js"
+  },
+  {
+    "revision": "0efa799a62b0f7633d1713580b2d4099",
+    "url": "/expense/static/media/logo.0efa799a.png"
+  }
+]);
